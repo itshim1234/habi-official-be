76f9017b7d68e16141f378c05ea8422eda500dc0
tejudharanesh <tejudharanesh1234@gmail.com>
2025-02-17 15:09:30 +0530

    RG

--- server.js
+++ server.js
@@ -97,17 +97,17 @@ app.post("/send-pdf", upload.single("invoicePdf"), async (req, res) => {
       subject: "Your Quotation PDF",
       html: `
        <div
-        style="justify-content: center; max-width: 450px; height: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; overflow: hidden; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); font-family: 'Poppins', Arial, sans-serif;">
+        style="justify-content: center; max-width: 450px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; overflow: hidden; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); font-family: 'Poppins', Arial, sans-serif;">
 
         <!-- Top Section with Centered Logo -->
-        <div style="justify-content: center; background: url('https://i.ibb.co/xqgZFt31/aaa.png') no-repeat center center; align-items: center; height: 100px; background-color: #ffffff; border-bottom: 1px dashed #7c7c7c;">
+        <div style="justify-content: center; background: url('https://i.ibb.co/xqgZFt31/aaa.png') no-repeat center center; align-items: center; height: 108px; background-color: #ffffff; border-bottom: 1px dashed #7c7c7c;">
             <img src="https://i.ibb.co/8gMCjJYc/Logo.png" alt="Habi Logo"
-                style="max-width: 180px; height: 60px; padding-top: 40px; justify-content: center; align-items: center;">
+                style="max-width: 180px; height: 60px; padding-top: 20px; justify-content: center; align-items: center;">
         </div>
 
         <!-- Content Section -->
         <div
-            style="position: relative; background: url('https://i.ibb.co/sd5KvcQg/bg.png') no-repeat center center; padding-top: 0px; padding-left: 35px; padding-right: 35px; text-align: center; color: #333333; height: 460px;">
+            style="position: relative; background: url('https://i.ibb.co/sd5KvcQg/bg.png') no-repeat center center; padding-top: 0px; padding-left: 35px; padding-right: 35px; text-align: center; color: #333333; height: 455px;">
             <h2 style="font-size: 18px; color: #7c7c7c; padding-top: 40px;">Dear ${name},</h2>
             <p style="font-size: 14px; padding-top: 8px; color: #7c7c7c;">
                 Thank you for reaching out. <br> Please find attached the quotation as per your request.
