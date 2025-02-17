c30915fde5c96558dd9070d24bf1401add2068c0
tejudharanesh <tejudharanesh1234@gmail.com>
2025-02-17 14:53:27 +0530

    efr

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.post("/send-pdf", upload.single("invoicePdf"), async (req, res) => {
         style="justify-content: center; max-width: 450px; height: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 10px; overflow: hidden; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1); font-family: 'Poppins', Arial, sans-serif;">
 
         <!-- Top Section with Centered Logo -->
-        <div style="justify-content: center; align-items: center; height: 100px;">
+        <div style="justify-content: center; align-items: center; height: 100px; background-color: #ffffff;">
             <img src="https://i.ibb.co/8gMCjJYc/Logo.png" alt="Habi Logo"
                 style="max-width: 180px; height: 60px; padding-top: 40px; justify-content: center; align-items: center;">
         </div>
@@ -108,8 +108,8 @@ app.post("/send-pdf", upload.single("invoicePdf"), async (req, res) => {
         <!-- Content Section -->
         <div
             style="position: relative; background: url('https://i.ibb.co/sd5KvcQg/bg.png') no-repeat center center; padding-top: 0px; padding-left: 35px; padding-right: 35px; text-align: center; color: #333333; height: 460px;">
-            <h2 style="font-size: 18px; color: #1a1a1a; padding-top: 40px;">Dear ${name},</h2>
-            <p style="font-size: 14px; padding-top: 8px; color: #1a1a1a;">
+            <h2 style="font-size: 18px; color: #7c7c7c; padding-top: 40px;">Dear ${name},</h2>
+            <p style="font-size: 14px; padding-top: 8px; color: #7c7c7c;">
                 Thank you for reaching out. <br> Please find attached the quotation as per your request.
             </p>
             <p style="font-size: 12px; font-style: italic; color: #7c7c7c; padding-top: 30px;">
@@ -122,12 +122,12 @@ app.post("/send-pdf", upload.single("invoicePdf"), async (req, res) => {
                     alt="">
             </p>
 
-            <p style="font-size: 14px; margin-bottom: 0em; padding-top: 25px; font-weight: bold; color: #1a1a1a;">
+            <p style="font-size: 14px; margin-bottom: 0em; padding-top: 25px; font-weight: bold; color: #7c7c7c;">
                 Best regards,</p>
-            <p style="font-size: 14px; margin-bottom: 0em; margin-top: 0.3em;">habi homes</p>
-            <p style="font-size: 14px; margin-bottom: 0em; margin-top: 0.3em;">9606210818</p>
-            <p style="font-size: 14px; margin-bottom: 0em; margin-top: 0.3em;"><a href="mailto:hello@habi.one"
-                    style="color: #000000; text-decoration: none;">hello@habi.one</a></p>
+            <p style="font-size: 14px; margin-bottom: 0em; margin-top: 0.3em; color: #7c7c7c;">habi homes</p>
+            <p style="font-size: 14px; margin-bottom: 0em; margin-top: 0.3em; color: #7c7c7c;">9606210818</p>
+            <p style="font-size: 14px; margin-bottom: 0em; margin-top: 0.3em; color: #7c7c7c;"><a href="mailto:hello@habi.one"
+                    style="color: #7c7c7c; text-decoration: none;">hello@habi.one</a></p>
 
             <div style="bottom: 0; text-align: center; padding-top: 30px;">
                 <p style="font-size: 14px; margin-bottom: 0em; margin-top: 0.3em; color: #7c7c7c;">↓ Quotation here ↓
